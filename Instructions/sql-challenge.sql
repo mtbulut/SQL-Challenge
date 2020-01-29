428b9824433440cf5d7dd740b90710fa60e7cb93
Memet Bulut <55109910+mtbulut@users.noreply.github.com>
2020-01-29 11:03:33 -0600

    sql-challenge modified

--- Instructions/sql-challenge.sql
+++ Instructions/sql-challenge.sql
@@ -2,5 +2,7 @@ use sakila;
 # getting the table actor.
 select * from actor;
 # 1a. Display the first and last names of all actors from the table actor.
-# deneme 
+select first_name, last_name from actor ;
+# 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
+select concat(first_name , ' ' , last_name) as "Actor Name" from actor;
 
