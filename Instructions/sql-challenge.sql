53e50799699919e3a6753dd728c2c2a01ee1e260
Memet Bulut <55109910+mtbulut@users.noreply.github.com>
2020-01-29 11:26:34 -0600

    2c completed

--- Instructions/sql-challenge.sql
+++ Instructions/sql-challenge.sql
@@ -1,8 +1,22 @@
 use sakila;
-# getting the table actor.
+-- getting the table actor.
 select * from actor;
-# 1a. Display the first and last names of all actors from the table actor.
+
+-- 1a. Display the first and last names of all actors from the table actor.
 select first_name, last_name from actor ;
-# 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
+
+-- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 select concat(first_name , ' ' , last_name) as "Actor Name" from actor;
 
+-- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
+select actor_id, first_name, last_name from actor
+where first_name = "Joe";
+
+-- 2b. Find all actors whose last name contain the letters GEN:
+select actor_id, first_name, last_name from actor
+where last_name like "%GEN%";
+
+-- 2c. Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order: 
+select actor_id, first_name, last_name from actor
+where last_name like "%LI%"
+order by last_name and first_name;
