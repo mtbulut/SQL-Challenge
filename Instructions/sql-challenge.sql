2654be3b91534ccdaf8c50cbfcf6d99e6d58ffec
Memet Bulut <55109910+mtbulut@users.noreply.github.com>
2020-01-30 12:41:45 -0600

    started 6a

--- Instructions/sql-challenge.sql
+++ Instructions/sql-challenge.sql
@@ -66,6 +66,19 @@ select * from actor
 where first_name = "GROUCHO" and last_name = 'WILLIAMS';
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 -- Hint: https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html
+describe sakila.address
+
+-- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. 
+-- Use the tables staff and address:
+
+-- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
+
+-- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
+
+-- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+
+-- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer.
+--  List the customers alphabetically by last name:
 
 
 
